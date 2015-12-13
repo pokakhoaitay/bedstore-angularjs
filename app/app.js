043d230e2599b5874c1dde405e312e37e44e6a97
tron.nguyen <pokakhoaitay@live.com>
2015-12-14 02:06:37 +0700

    new structure for views, .htaccess working with route

--- app/app.js
+++ app/app.js
@@ -2,11 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.home',
+    'myApp.about',
+    'myApp.version',
+    'ngMaterial'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
+    $routeProvider.otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
 }]);
