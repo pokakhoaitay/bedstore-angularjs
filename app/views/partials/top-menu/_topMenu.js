90d7b73b997279b1e761a490c7b10816bbc76055
tron.nguyen <pokakhoaitay@live.com>
2015-12-16 01:02:29 +0700

    Create menu

--- app/views/partials/top-menu/_topMenu.js
+++ app/views/partials/top-menu/_topMenu.js
@@ -5,6 +5,20 @@
 
 angular.module('myApp._topMenu',['ngRoute','ngMaterial'])
 
-    .controller('_TopMenuCtrl', ['$scope',function ($scope) {
-        $scope.value=123
+    .controller('_TopMenuCtrl', ['$scope','$window',function ($scope,$window) {
+        $scope.myTest='Nguyen Hong Tron';
+        angular.element($window).bind('scroll', function () {
+            if (this.pageYOffset >= 32) {
+              var test=  angular.element(document.querySelector('md-toolbar.main-toolbar'));
+
+                $scope.$apply(function () {
+                    $scope.isFixed=true;
+                });
+            } else {
+                $scope.$apply(function () {
+                    $scope.isFixed=false;
+                });
+
+            }
+        })
     }]);
