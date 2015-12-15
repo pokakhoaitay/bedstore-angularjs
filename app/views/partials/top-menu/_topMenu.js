9a2f7ebd1463e3456fc88512211074541aa59348
tron.nguyen <pokakhoaitay@live.com>
2015-12-15 11:14:04 +0700

    Apply sass compiler, understand theming in angular material

---
+++ app/views/partials/top-menu/_topMenu.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Poka on 12/14/2015.
+ */
+'use strict';
+
+angular.module('myApp._topMenu',['ngRoute','ngMaterial'])
+
+    .controller('_TopMenuCtrl', ['$scope',function ($scope) {
+        $scope.value=123
+    }]);
