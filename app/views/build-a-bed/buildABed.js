75f10ca6b00b165a126ae922c5779c9bda600fc0
tron.nguyen <pokakhoaitay@live.com>
2015-12-20 18:15:00 +0700

    temporality build image ok

--- app/views/build-a-bed/buildABed.js
+++ app/views/build-a-bed/buildABed.js
@@ -1,12 +1,18 @@
 'use strict';
 
-angular.module('myApp.buildABed',['ngRoute'])
+angular.module('myApp.buildABed', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/build-a-bed',{
-            templateUrl:'views/build-a-bed/buildABed.html',
-            controller:'BuildABedCtrl'
+        $routeProvider.when('/build-a-bed', {
+            templateUrl: 'views/build-a-bed/buildABed.html',
+            controller: 'BuildABedCtrl'
         })
     }])
-.controller('BuildABedCtrl',['$scope', function ($scope) {
+    .controller('BuildABedCtrl', function ($scope) {
+        $scope.bedData={};
+        $scope.bedData.cabinet=2;
 
-}])
+        $scope.changeCabinet= function (value) {
+            $scope.bedData.cabinet=value;
+        }
+
+    })
