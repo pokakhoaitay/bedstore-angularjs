8be47a753b5be6ef0e45491f55a29fc6528c2752
pokakhoaitay <pokakhoaitay@live.com>
2016-02-14 21:48:06 +0700

    getAddress

--- app/proxy/lib/core/app.utils.php
+++ app/proxy/lib/core/app.utils.php
@@ -10,5 +10,12 @@ final class Utils{
     public static function calculateHMAC($data, $key){
         return base64_encode(hash_hmac('sha1',$data,$key));
     }
+    public static function getAddress()
+    {
+        /*** check for https ***/
+        $protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+        /*** return the full address ***/
+        return $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    }
 
 }
